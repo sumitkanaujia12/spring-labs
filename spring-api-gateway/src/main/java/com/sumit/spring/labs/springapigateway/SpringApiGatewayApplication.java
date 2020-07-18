87a80c97af6d0b5b1d61456beead1a339fd6addc
Sumit Kanaujia <Sumit.Kanaujia@walmartlabs.com>
2020-07-18 14:34:26 +0530

    api gateway setup

---
+++ spring-api-gateway/src/main/java/com/sumit/spring/labs/springapigateway/SpringApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.sumit.spring.labs.springapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiGatewayApplication.class, args);
+    }
+
+}
---
+++ spring-api-gateway/src/test/java/com/sumit/spring/labs/springapigateway/SpringApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumit.spring.labs.springapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
